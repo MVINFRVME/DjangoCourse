2902fc3ea2a7a7a435eb82521e84b496abcfaed6
MVINFRVME <galgamet1337@gmail.com>
2025-02-25 06:18:29 +0500

    3.4 lesson done

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,9 +1,8 @@
-from itertools import product
 from django.contrib.auth.models import Group
 from timeit import default_timer
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Order
 
 
 def shop_index(request: HttpRequest):
@@ -28,6 +27,13 @@ def groups_list(request: HttpRequest):
 
 def products_list(request: HttpRequest):
     context = {
-        'products': Product.objects.all()
+        'products': Product.objects.all(),
     }
     return render(request, 'shopapp/products-list.html', context=context)
+
+
+def orders_list(request: HttpRequest):
+    context = {
+        'orders': Order.objects.select_related('user').prefetch_related('products').all(),
+    }
+    return render(request, 'shopapp/orders-list.html', context=context)
