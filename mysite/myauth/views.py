f6a1c5c912c3597c5df6afe99d978f4b75255182
MVINFRVME <galgamet1337@gmail.com>
2025-03-28 06:57:59 +0500

    9.1, 9.2 lessons Done

---
+++ mysite/myauth/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
+
+def login_view(request: HttpRequest) -> HttpResponse:
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            return redirect('/admin/')
+
+        return render(request, 'myauth/login.html')
+
+    user_name = request.POST['username']
+    password = request.POST['password']
+
+    user = authenticate(request, username=user_name, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('/admin/')
+
+    return render(request, 'myauth/login.html', {'error': 'Invalid login credentials'})
+
